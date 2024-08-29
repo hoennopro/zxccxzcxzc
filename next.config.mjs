7430a927b019e5c1797da6c60a6bacc760551f4d
hoennopro <hoangqazplm@gmail.com>
2024-08-29 08:05:07 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en", "vi"],
+    defaultLocale: "en",
+  },
+};
 
 export default nextConfig;
